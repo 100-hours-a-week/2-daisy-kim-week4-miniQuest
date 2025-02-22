9282b380436249e0d40e80031a68cf6185a0465d
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-02-22 18:21:20 +0900

    [feat] 비동기, 논블로킹 적용
    - 순차처리하는 js의 특징으로 문장 출력
    - setTimeout함수를 적용하여 비동기 처리
    -> End 출력을 위해 promise사용
    - 4-2 개인적으로 헷갈려서 적은 주석 추가

--- 4-2/app.js
+++ 4-2/app.js
@@ -5,3 +5,8 @@ const user = new User('daisy', 24);
 const result = substract(add(2, 3), 1);
 console.log(user.greet());
 console.log('연산 결과 : ', result);
+
+/*
+greet()함수에서 return 없이 콘솔 출력을 하면
+app.js에서는 콘솔 함수 없이 greet함수 호출만 하면 됨
+*/
